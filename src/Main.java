d3f2107dc79c61f370eb9ff7fe353c3183cfe32b
michaelwood9 <3mawood@gmail.com>
2022-09-06 22:19:06 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String mystr;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello world");
+        mystr = scanner.next();
+        System.out.println(mystr);
+        if(mystr.equals("hello")){
+            System.out.println("hello stranger");
+        }
+
+
+    }
+}
